6e9db856e0fe56c1c19d0661ed9fea626fb0ad12
ai-dbeta <tatianitapipe@gmail.com>
2025-01-31 03:55:11 -0500

    Update examples.js

--- examples.js
+++ examples.js
@@ -9,3 +9,13 @@ const result = math.add(a, b);
 
 console.log('Result of matrix addition:');
 console.log(result);
+
+// examples/matrixAddition.js
+const math = require('mathjs');
+
+const a = math.matrix([[1, 2], [3, 4]]);
+const b = math.matrix([[5, 6], [7, 8]]);
+const result = math.add(a, b);
+
+console.log('Result of matrix addition:');
+console.log(result);
