35c88a2cda77d8ddef897d9109b2e2d5a43804f1
ai-dbeta <tatianitapipe@gmail.com>
2025-01-31 03:52:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ const result = math.add(a, b);
 
 console.log('Result of matrix addition:');
 console.log(result);
+
+// index.js
+const math = require('mathjs');
+
+function addMatrices(a, b) {
+  try {
+    return math.add(a, b);
+  } catch (error) {
+    console.error('Error adding matrices:', error.message);
+    return null;
+  }
+}
+
+const a = math.matrix([[1, 2], [3, 4]]);
+const b = math.matrix([[5, 6], [7, 8]]);
+const result = addMatrices(a, b);
+
+if (result) {
+  console.log('Result of matrix addition:');
+  console.log(result);
+}
