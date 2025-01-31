0df3010bfab6fb583e7c203ae7389a4008add36e
ai-dbeta <tatianitapipe@gmail.com>
2025-01-31 02:08:02 -0500

    Create examples.js

---
+++ examples.js
@@ -0,0 +1,11 @@
+// examples.js
+
+const math = require('mathjs');
+
+// Example usage of Math.js
+const a = math.matrix([[1, 2], [3, 4]]);
+const b = math.matrix([[5, 6], [7, 8]]);
+const result = math.add(a, b);
+
+console.log('Result of matrix addition:');
+console.log(result);
